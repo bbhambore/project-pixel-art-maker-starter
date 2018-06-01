5d8b7203f4aa88bdfc9409fb6dbeda89c0b44f92
Bharath Bhambore <bbhambore@gmail.com>
2018-06-01 16:31:17 +0530

    Add necessary comments

--- designs.js
+++ designs.js
@@ -3,19 +3,24 @@
 
 // When size is submitted by the user, call makeGrid()
 
+// Add grid based on user input
 $('#submit').click(function makeGrid(event) {
+    //Prevent the page from being refreshed
     event.preventDefault();
 
+    //Remove the existing grid if present
     if($('#pixelCanvas>tr').length) {
         console.log("Removing the existing canvas");
         $('#pixelCanvas>tr').remove();
     }
 
+    //Get the input height and width
     let rows = $('#inputHeight').val();
     let columns = $('#inputWidth').val();
     let data = '';
     console.log("Grid size: "+rows+"*"+columns);
 
+    //Construct the table
     for (let i=0;i<rows;i++) {
         data+= '<tr>';
         for (let j=0;j<columns;j++) {
@@ -26,6 +31,7 @@ $('#submit').click(function makeGrid(event) {
     $('#pixelCanvas').append(data);
 });
 
+//Add color to the grid
 $('#pixelCanvas').on('click', 'td', function applyColor(event) {
     $(this).css('background-color',$('#colorPicker').val());
 });
