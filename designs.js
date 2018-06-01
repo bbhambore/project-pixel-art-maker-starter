180707e53466f265c4905d58d9f12dd104590652
Bharath Bhambore <bbhambore@gmail.com>
2018-06-01 11:22:29 +0530

    feat: Accept dimensions from user and create grid

--- designs.js
+++ designs.js
@@ -3,8 +3,25 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$('#submit').click(function makeGrid(event) {
+    event.preventDefault();
 
-// Your code goes here!
+    if($('#pixelCanvas>tr').length) {
+        console.log("Removing the existing canvas");
+        $('#pixelCanvas>tr').remove();
+    }
 
-}
+    let rows = $('#inputHeight').val();
+    let columns = $('#inputWidth').val();
+    let data = '';
+    console.log("Grid size: "+rows+"*"+columns);
+
+    for (let i=0;i<rows;i++) {
+        data+= '<tr>';
+        for (let j=0;j<columns;j++) {
+            data+= '<td></td>';
+        }
+        data+= '</tr>';
+    }
+    $('#pixelCanvas').append(data);
+});
